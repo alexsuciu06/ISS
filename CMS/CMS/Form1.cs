fd724b65b54223a11b2fa8e860ff214e6a26528c
fluturbogdan <cragoliof@gmail.com>
2017-05-13 14:19:20 +0300

    Login Window
    Create Mock

--- CMS/CMS/Form1.cs
+++ CMS/CMS/Form1.cs
@@ -10,11 +10,67 @@ using System.Windows.Forms;
 
 namespace CMS
 {
-    public partial class Form1 : Form
+    public partial class LoginWindow : Form
     {
-        public Form1()
+        public LoginWindow()
         {
             InitializeComponent();
+            UsernameTextbox.ForeColor = SystemColors.GrayText;
+            UsernameTextbox.Text = "Please Enter Your Username";
+            PasswordTextbox.ForeColor = SystemColors.GrayText;
+            PasswordTextbox.Text = "Please Enter Your Password";
+            this.UsernameTextbox.Leave += new System.EventHandler(this.UsernameTextbox_Leave);
+            this.UsernameTextbox.Enter += new System.EventHandler(this.UsernameTextbox_Enter);
+            this.PasswordTextbox.Leave += new System.EventHandler(this.PasswordTextbox_Leave);
+            this.PasswordTextbox.Enter += new System.EventHandler(this.PasswordTextbox_Enter);
+        }
+
+        private void UsernameTextbox_Leave(object sender, EventArgs e)
+        {
+            if (UsernameTextbox.Text.Length == 0)
+            {
+                UsernameTextbox.Text = "Please Enter Your Username";
+                UsernameTextbox.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void UsernameTextbox_Enter(object sender, EventArgs e)
+        {
+            if (UsernameTextbox.Text == "Please Enter Your Username")
+            {
+                UsernameTextbox.Text = "";
+                UsernameTextbox.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void PasswordTextbox_Leave(object sender, EventArgs e)
+        {
+            if (PasswordTextbox.Text.Length == 0)
+            {
+                PasswordTextbox.Text = "Please Enter Your Password";
+                PasswordTextbox.ForeColor = SystemColors.GrayText;
+                PasswordTextbox.PasswordChar = '\0';
+            }
+        }
+
+        private void PasswordTextbox_Enter(object sender, EventArgs e)
+        {
+            if (PasswordTextbox.Text == "Please Enter Your Password")
+            {
+                PasswordTextbox.Text = "";
+                PasswordTextbox.ForeColor = SystemColors.WindowText;
+                PasswordTextbox.PasswordChar = '*';
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
