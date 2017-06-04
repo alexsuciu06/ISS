9ac6c26c018f4aa61d171aa8c03375542cc62f66
mariaflorescu <mariagabriela902@yahoo.com>
2017-06-04 19:59:09 +0300

    bidding

---
+++ CMS/CMSClient/Controllers/BiddingPageController.cs
@@ -0,0 +1,41 @@
+﻿using CMSServer;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMS
+{
+    public class BiddingPageController
+    {
+        private IServer server;
+
+        public BiddingPageController(IServer server)
+        {
+            this.server = server;
+        }
+
+        public List<Paper> GetAllPapers()
+        {
+            try
+            {
+                return server.GetAllPapers();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return null;
+        }
+
+        public void AddBidding(int idPaper, string bidEnum, User currentUser)
+        {
+            Paper paper = server.findPaperById(idPaper);
+            BidEnum bide = (BidEnum)Enum.Parse(typeof(BidEnum), bidEnum);
+            server.AddBidding(paper, bide, currentUser);
+        }
+    }
+}
